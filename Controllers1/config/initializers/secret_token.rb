3455d5cf2b8edf2ec85323afb8c27c7f7e13ea04
Tamara Solorzano <st.tamara@gmail.com>
2013-04-12 15:43:10 -0700

    Ruby on Rails - Controllers - Exercise 1
    Create two controllers called Users and Posts
    For each controller, create methods index and new and appropriate view
    files
    /users/index will later display all the users and users/new will later
    display a form that allows someone to create a new user
    /posts/index will later display all the posts and posts/new will later
    display a form that allows someone to create a new post
    Create two layouts (users.html.erb and posts.html.erb)
    When you go to users/index or users/new, have it have a completely
    different look/layout than the pages in the posts controller.
    For the users layout
    use users.js or users.js.coffee
    use users.css or users.css.scss
    do NOT call application.js or application.css
    For the posts layout
    use posts.js or posts.js.coffee
    use posts.css or posts.css.scss
    do NOT call application.js or application.css
    For each /new view (/users/new and /posts/new), create a form with
    plain HTML (do not use any form helpers) just so that it looks nice.
    Modify your layouts so that the view file can pass a parameter/text to
    the layout allowing the layout to display different titles for each
    view page.  You can make the title tag for each page as follows
    /users/index - Display all users
    /users/new - Add a new user
    /posts/index - Display all posts
    /posts/new - Add a new post

---
+++ Controllers1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Controllers1::Application.config.secret_token = 'c4199cf581894dd788a9b1dbe63ee1b1a82cb1272dbd66180dbab0f0d04b5fb820101b1d894edfacb4cdb7816872dad192de35dcc3eb407150ac3361ddfe5837'
